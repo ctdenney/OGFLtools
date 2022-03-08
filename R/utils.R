d11d6e1ef1c163912868d3341270de478deadf3c
Christian Denney <christian.denney@gmail.com>
2022-03-08 11:53:12 -0800

    function updates
    added sn() function
    added error/warning about sallims to sr_to_sal() function

--- R/utils.R
+++ R/utils.R
@@ -3,6 +3,10 @@
 
 }
 
+sn = function(x) {
+  length(unique(x))
+}
+
 ec_to_sal = function(temp, cond){
   if (any(temp > 35, na.rm = T)) {
     warning('Temperature is high, ensure that units are in degrees C', call. = F, immediate. = T)
@@ -79,11 +83,15 @@ sal_to_spc = function(sal){
 # }
 
 sr_to_sal = function(sr, sr1 = 0.70576, sr2 = 0.70918,con1 = 95, con2 = 6819,sal1 = 0.11,sal2 = 31.8, sallim_high = max(sal1, sal2), fill_high = "NA", sallim_low = min(sal1,sal2), fill_low = "NA",suppress.warnings = F){
+
+  if (sallim_low < min(sal1, sal2) | sallim_high > max(sal1,sal2)) {
+    stop('One or both of your sallims are outside the bounds of your endmembers')
+  }
+  if (is.na(sallim_high) | is.na(sallim_low)) stop('You have not set one or both of the salinity limit values(sallim_high and sallim_low arguments)')
   if (any(sr < min(sr1, sr2)| sr > max(sr1, sr2), na.rm = T) & suppress.warnings == F) {
     warning('Some of your measured strontium isotope ratio values are outside the bounds of your two endmembers, make sure that sr1 and sr2 are set correctly',
             call. = F, immediate. = T)
   }
-  if (is.na(sallim_high) | is.na(sallim_low)) stop('You have not set one or both of the salinity limit values(sallim_high and sallim_low arguments)')
   sal = (((sal1*sr2*con2) - (sal1*sr*con2) - (sal2*sr2*con2) + (sal2*sr*con2))/
            ((sr*con1) - (sr*con2) - (sr1*con1) + (sr2*con2))) + sal2
   if((fill_high != 'NA' & is.numeric(fill_high) == F) | (fill_low != 'NA' & is.numeric(fill_low) == F)) stop('you have not set an appropriate fill value. Fill must be either \'NA\' or a numeric value, for both fill_low and fill_high.')
